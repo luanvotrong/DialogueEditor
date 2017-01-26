ea656bfe8496b10c7bc42f02bd615093a9435e04
luanvotrong <trongluan@TrongLuans-MacBook-Pro.local>
2017-01-26 20:53:01 +0700

    - Update

--- JS/JS/core/Graphic.js
+++ JS/JS/core/Graphic.js
@@ -1,9 +1,15 @@
 function Graphic() {
+    this.canvas = null;
+    this.context = null;
     this.screenW = 0;
     this.screenH = 0;
-    
-    this.Init = function() {
-        console.log("fuck");
+
+    this.Init = function () {
+        this.canvas = document.getElementById("myCanvas");
+        this.context = this.canvas.getContext("2d");
+        this.screenW = this.context.canvas.clientWidth;
+        this.screenH = this.context.canvas.clientHeight;
+        console.log("Finished init Graphic w: " + this.screenW + " h: " + this.screenH);
     }
 }
 
--- JS/JS/core/Main.js
+++ JS/JS/core/Main.js
@@ -1,7 +1,25 @@
 function Main() {
-    this.Init = function() {
+    var FPS_INTERVAL = 1000 / 20;
+    this.Init = function () {
         Graphic.Init();
+        last = Date.now();
+        this.run();
     }
-}
 
+    var last = Date.now();
+    this.run = function () {
+        //RUNNING
+        var now = Date.now();
+        console.log("running");
+        if (now - last >= FPS_INTERVAL) {
+            update();
+            draw();
+        }
+    }
+    var run = function () {}
+
+    var update = function (dt) {}
+
+    var draw = function () {}
+}
 var Main = new Main();
