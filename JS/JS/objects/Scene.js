170d509f3835f5c3a4a676f36b8cd2935ca6ab70
luanvotrong <trongluan@TrongLuans-MacBook-Pro.local>
2017-01-30 20:49:44 +0700

    - Update

--- JS/JS/objects/Scene.js
+++ JS/JS/objects/Scene.js
@@ -7,84 +7,30 @@ function Lamp(min, max, v) {
 }
 
 function Scene() {
-    var SCROLL_BASE = 0.02;
-    var SCROLL_DE = 0.3;
-
-    var dragOffsetX = 0;
-    var dragOffsetY = 0;
-    var offsetX = 0;
-    var offsetY = 0;
-
-    var isDragging = false;
-    var shouldCheckPress = false;
-    var dialogueBoxes = [];
-
-    var globalScale = 1;
-    var scaleSpeed = 0;
-
+    var sceneLayer = new SceneLayer();
+    
     this.update = function (dt) {
-        globalScale -= scaleSpeed * dt;
-        globalScale = Lamp(0.3, 1, globalScale);
-        offsetX = (1 - globalScale) * Graphic.screenW / 2;
-        offsetY = (1 - globalScale) * Graphic.screenH / 2;
-
-        scaleSpeed *= SCROLL_DE;
-        scaleSpeed = (Math.abs(scaleSpeed) < 0.1) ? 0 : scaleSpeed;
-
-        var len = dialogueBoxes.length;
-        for (var i = 0; i < len; i++) {
-            dialogueBoxes[i].update(dt);
-        }
+        sceneLayer.update(dt);
     }
 
     this.draw = function () {
-        var context = Graphic.context;
-        context.save();
-        context.translate(offsetX + dragOffsetX, offsetY + dragOffsetY);
-        context.scale(globalScale, globalScale);
-
-        var len = dialogueBoxes.length;
-        for (var i = 0; i < len; i++) {
-            dialogueBoxes[i].draw();
-        }
-
-        context.scale(1, 1);
-        context.restore();
+        sceneLayer.draw();
     }
-
+    
     this.onScroll = function (delta) {
-        scaleSpeed = delta * SCROLL_BASE;
-        console.log(delta);
+        sceneLayer.onScroll(delta);
     }
 
-
-    var preX = 0;
-    var preY = 0;
     this.onMouseDown = function (x, y) {
-        isDragging = true;
-        shouldCheckPress = true;
-        preX = x;
-        preY = y;
+        sceneLayer.onMouseDown(x, y);
     }
 
     this.onMouseMove = function (x, y) {
-        if (isDragging) {
-            dragOffsetX += (x - preX);
-            dragOffsetY += (y - preY);
-            preX = x;
-            preY = y;
-        }
-        shouldCheckPress = false;
+        sceneLayer.onMouseMove(x, y);
     }
 
     this.onMouseUp = function (x, y) {
-        if (shouldCheckPress) {
-            var dialogueBox = new DialogueBox();
-            dialogueBox.Init(x - dragOffsetX, y - dragOffsetY);
-            dialogueBoxes.push(dialogueBox);
-        }
-        isDragging = false;
-        shouldCheckPress = false;
+        sceneLayer.onMouseUp(x, y);
     }
 }
 
@@ -108,5 +54,4 @@ RootObjectPool.AddObject(Scene);
 //
 //this.onMouseUp = function (x, y) {
 //
-//}
 //}
---
+++ JS/JS/objects/SceneLayer.js
@@ -0,0 +1,81 @@
+function SceneLayer() {
+    var SCROLL_BASE = 0.02;
+    var SCROLL_DE = 0.3;
+
+    var dragOffsetX = 0;
+    var dragOffsetY = 0;
+    var offsetX = 0;
+    var offsetY = 0;
+
+    var isDragging = false;
+    var shouldCheckPress = false;
+    var dialogueBoxes = [];
+
+    var globalScale = 1;
+    var scaleSpeed = 0;
+
+    this.update = function (dt) {
+        globalScale -= scaleSpeed * dt;
+        globalScale = Lamp(0.3, 1, globalScale);
+        offsetX = (1 - globalScale) * Graphic.screenW / 2;
+        offsetY = (1 - globalScale) * Graphic.screenH / 2;
+
+        scaleSpeed *= SCROLL_DE;
+        scaleSpeed = (Math.abs(scaleSpeed) < 0.1) ? 0 : scaleSpeed;
+
+        var len = dialogueBoxes.length;
+        for (var i = 0; i < len; i++) {
+            dialogueBoxes[i].update(dt);
+        }
+    }
+
+    this.draw = function () {
+        var context = Graphic.context;
+        context.save();
+        context.translate(offsetX + dragOffsetX, offsetY + dragOffsetY);
+        context.scale(globalScale, globalScale);
+
+        var len = dialogueBoxes.length;
+        for (var i = 0; i < len; i++) {
+            dialogueBoxes[i].draw();
+        }
+
+        context.scale(1, 1);
+        context.restore();
+    }
+
+    this.onScroll = function (delta) {
+        scaleSpeed = delta * SCROLL_BASE;
+        console.log(delta);
+    }
+
+
+    var preX = 0;
+    var preY = 0;
+    this.onMouseDown = function (x, y) {
+        isDragging = true;
+        shouldCheckPress = true;
+        preX = x;
+        preY = y;
+    }
+
+    this.onMouseMove = function (x, y) {
+        if (isDragging) {
+            dragOffsetX += (x - preX);
+            dragOffsetY += (y - preY);
+            preX = x;
+            preY = y;
+        }
+        shouldCheckPress = false;
+    }
+
+    this.onMouseUp = function (x, y) {
+        if (shouldCheckPress) {
+            var dialogueBox = new DialogueBox();
+            dialogueBox.Init(x - dragOffsetX, y - dragOffsetY);
+            dialogueBoxes.push(dialogueBox);
+        }
+        isDragging = false;
+        shouldCheckPress = false;
+    }
+}
